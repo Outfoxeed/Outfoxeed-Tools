837acfd11642b0a79b6a054d15efe5cbdbb429ea
Outfoxeed <49041599+Outfoxeed@users.noreply.github.com>
2026-04-15 20:59:36 +0200

    [ADD] Generic EventBus

---
+++ Runtime/Generics/EventBus.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OutfoxeedTools
+{
+    public static class EventBus<T> where T : struct
+    {
+        private static event Action<T> Callback = null;
+        
+        public static void Raise(T eventValue)
+        {
+            Callback?.Invoke(eventValue);
+        }
+
+        public static void Register(Action<T> callback)
+        {
+            Callback += callback;
+        }
+
+        public static void Unregister(Action<T> callback)
+        {
+            Callback -= callback;
+        }
+
+        public static void Clear()
+        {
+            Callback = null;
+        }
+    }
+}
